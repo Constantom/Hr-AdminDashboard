0f1d53772dee5f5f7eec5470a1532d1e16c6961f
Constance Essien <Constancetom82@gmail.com>
2026-02-02 17:29:41 -0800

    Front end adjustment

--- public/js/ui.js
+++ public/js/ui.js
@@ -36,3 +36,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 15);
   });
 
+const menuBtn = document.getElementById('menuBtn');
+const sidebar = document.querySelector('.sidebar');
+
+if (menuBtn && sidebar) {
+  menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+  });
+}
